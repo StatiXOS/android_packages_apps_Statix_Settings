e9613f1b573fae348379abd0bb5cabd0fdb2d7a6
Anay Wadhera <anay1018@gmail.com>
2022-08-17 11:09:18 +0900

    StatixSettings: drop Google parts
    Change-Id: Ib7efe8a983bf7ba4b78dd3162d0b54270fc81d99
    Reviewed-on: https://review.statixos.com/c/android_packages_apps_Statix_Settings/+/8245
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
+++ src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
@@ -5,29 +5,7 @@ import android.content.Context;
 import com.android.settings.overlay.FeatureFactoryImpl;
 import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.accounts.AccountFeatureProvider;
-import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
-import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
-
-    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
-    private AccountFeatureProvider mAccountFeatureProvider;
-
-    @Override
-    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
-        if (mPowerUsageFeatureProvider == null) {
-            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
-                    context.getApplicationContext());
-        }
-        return mPowerUsageFeatureProvider;
-    }
-
-    @Override
-    public AccountFeatureProvider getAccountFeatureProvider() {
-        if (mAccountFeatureProvider == null) {
-            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
-        }
-        return mAccountFeatureProvider;
-    }
 
 }
