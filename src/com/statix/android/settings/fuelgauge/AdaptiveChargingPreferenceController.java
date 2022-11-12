6305fc8b1768cbfad8fdd426193b845aaae9f473
Anay Wadhera <anay1018@gmail.com>
2022-11-12 00:25:41 -0500

    StatixSettings: Implement Adaptive Charging for google battery HAL
    Change-Id: I2e87971c3b199a8cc594b2422c107256fadddb3e

---
+++ src/com/statix/android/settings/fuelgauge/AdaptiveChargingPreferenceController.java
@@ -0,0 +1,56 @@
+package com.statix.android.settings.fuelgauge;
+
+import android.content.Context;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import com.statix.android.systemui.adaptivecharging.AdaptiveChargingManager;
+
+public class AdaptiveChargingPreferenceController extends TogglePreferenceController {
+
+    private AdaptiveChargingManager mAdaptiveChargingManager;
+    private boolean mChecked;
+
+    public AdaptiveChargingPreferenceController(Context context, String key) {
+        super(context, key);
+        mAdaptiveChargingManager = new AdaptiveChargingManager(context);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mAdaptiveChargingManager.isAvailable() ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return mAdaptiveChargingManager.getEnabled();
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        mChecked = isChecked();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_battery;
+    }
+
+    @Override
+    public boolean setChecked(boolean checked) {
+        mAdaptiveChargingManager.setEnabled(checked);
+        if (!checked) {
+            mAdaptiveChargingManager.setAdaptiveChargingDeadline(-1);
+        }
+        if (mChecked != checked) {
+            mChecked = checked;
+            return true;
+        }
+        return true;
+    }
+
+}
