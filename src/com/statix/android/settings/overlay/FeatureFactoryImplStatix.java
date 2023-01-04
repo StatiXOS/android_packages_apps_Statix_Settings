d672c8a6b64cb53e5e42196cee8cd06bafb2fb7b
Sourajit Karmakar <sourajitk@statixos.com>
2023-01-04 23:36:26 +0000

    StatixSettings: Implement PowerUsageFeatureProvider from 11
    Change-Id: I5316c1a34c31551bb68c0a670304a29cc5ac9482

--- src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
+++ src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
@@ -4,16 +4,19 @@ import android.content.Context;
 
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.fuelgauge.BatteryStatusFeatureProvider;
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.overlay.FeatureFactoryImpl;
 
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 
 import com.statix.android.settings.fuelgauge.BatteryStatusFeatureProviderStatixImpl;
+import com.statix.android.settings.fuelgauge.PowerUsageFeatureProviderStatixImpl;
 
 public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
 
     private AccountFeatureProvider mAccountFeatureProvider;
     private BatteryStatusFeatureProvider mBatteryStatusFeatureProvider;
+    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
 
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
@@ -29,6 +32,14 @@ public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
             mBatteryStatusFeatureProvider = new BatteryStatusFeatureProviderStatixImpl(context);
         }
         return mBatteryStatusFeatureProvider;
+    }
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderStatixImpl(context);
+        }
+        return mPowerUsageFeatureProvider;
     }
 
 }
