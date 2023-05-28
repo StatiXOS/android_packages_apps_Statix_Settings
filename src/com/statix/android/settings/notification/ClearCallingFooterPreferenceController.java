007cff51d070cb41027ed6b2d9f58596195de5ac
Anay Wadhera <anay1018@gmail.com>
2023-05-28 17:08:05 -0400

    StatixSettings: Import Clear Calling
    Change-Id: I1bbdd39dbb7bd772efb564d355b696c1657254cd

---
+++ src/com/statix/android/settings/notification/ClearCallingFooterPreferenceController.java
@@ -0,0 +1,38 @@
+package com.statix.android.settings.notification;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.text.TextUtils;
+import android.view.View;
+import androidx.preference.PreferenceScreen;
+import androidx.window.R;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.HelpUtils;
+import com.android.settingslib.widget.FooterPreference;
+
+public class ClearCallingFooterPreferenceController extends BasePreferenceController {
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    public ClearCallingFooterPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen preferenceScreen) {
+        super.displayPreference(preferenceScreen);
+        FooterPreference footerPreference = (FooterPreference) preferenceScreen.findPreference(getPreferenceKey());
+        final String string = mContext.getString(R.string.clear_calling_footer_learn_more_link);
+        if (footerPreference == null || TextUtils.isEmpty(string)) {
+            return;
+        }
+        footerPreference.setLearnMoreAction(new View.OnClickListener() {
+            @Override
+            public final void onClick(View view) {
+                mContext.startActivity(HelpUtils.getHelpIntent(mContext, mPreferenceKey, ""));
+            }
+        });
+    }
+}
