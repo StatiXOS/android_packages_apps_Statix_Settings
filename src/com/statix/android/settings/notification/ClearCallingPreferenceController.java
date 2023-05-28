ab2a17c5235d21932060ee35b88637f45af0e80b
Anay Wadhera <anay1018@gmail.com>
2023-05-28 17:08:05 -0400

    StatixSettings: Import Clear Calling
    Change-Id: I1bbdd39dbb7bd772efb564d355b696c1657254cd

---
+++ src/com/statix/android/settings/notification/ClearCallingPreferenceController.java
@@ -0,0 +1,27 @@
+package com.statix.android.settings.notification;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class ClearCallingPreferenceController extends BasePreferenceController {
+    private static final String CCA_ENABLED_FLAG = "CcaConfig__is_enabled";
+    private static final String CCA_STATUS_OFF = "cca_pixel_enabled=false";
+    private final AudioManager mAudioManager;
+
+    public ClearCallingPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+        mAudioManager = (AudioManager) context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (mContext.getResources().getBoolean(R.bool.config_clear_calling_enabled)) {
+            return AVAILABLE;
+        }
+        return UNSUPPORTED_ON_DEVICE;
+    }
+}
