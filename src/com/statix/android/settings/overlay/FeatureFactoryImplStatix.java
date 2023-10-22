4c557d7c3f9ec48726f7eda164308603dfcd5178
Anay Wadhera <anay1018@gmail.com>
2023-10-22 11:52:05 -0400

    Revert "StatixSettings: Implement PowerUsageFeatureProvider from 11"
    This reverts commit d672c8a6b64cb53e5e42196cee8cd06bafb2fb7b.
    
    Change-Id: Ifa58747bef5ab6839c19bf845f05a44e642cc055

--- src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
+++ src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.java
@@ -4,18 +4,15 @@ import android.content.Context;
 
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.fuelgauge.BatteryStatusFeatureProvider;
-import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.overlay.FeatureFactoryImpl;
 
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 import com.statix.android.settings.fuelgauge.BatteryStatusFeatureProviderStatixImpl;
-import com.statix.android.settings.fuelgauge.PowerUsageFeatureProviderStatixImpl;
 
 public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
 
     private AccountFeatureProvider mAccountFeatureProvider;
     private BatteryStatusFeatureProvider mBatteryStatusFeatureProvider;
-    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
 
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
@@ -31,13 +28,5 @@ public final class FeatureFactoryImplStatix extends FeatureFactoryImpl {
             mBatteryStatusFeatureProvider = new BatteryStatusFeatureProviderStatixImpl(context);
         }
         return mBatteryStatusFeatureProvider;
-    }
-
-    @Override
-    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
-        if (mPowerUsageFeatureProvider == null) {
-            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderStatixImpl(context);
-        }
-        return mPowerUsageFeatureProvider;
     }
 }
