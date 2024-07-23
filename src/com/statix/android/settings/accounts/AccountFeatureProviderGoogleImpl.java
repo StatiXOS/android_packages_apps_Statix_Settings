4c4b7124e1b58cd55e65460f21ade7e46979bb2d
ralph950412 <ralph950412@gmail.com>
2024-07-23 22:41:59 +0800

    experiment
    Change-Id: I8ebb66e09a1dce812ad341274f7d4e7129edab9b

--- src/com/statix/android/settings/accounts/AccountFeatureProviderGoogleImpl.java
+++ src/com/statix/android/settings/accounts/AccountFeatureProviderGoogleImpl.java
@@ -5,15 +5,17 @@ import android.accounts.AccountManager;
 import android.content.Context;
 
 import com.android.settings.accounts.AccountFeatureProvider;
+import com.android.settings.R;
+import com.android.settings.overlay.FeatureFactory;
 
 public class AccountFeatureProviderGoogleImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return "com.google";
+        return FeatureFactory.getAppContext().getString(R.string.account_type);
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return AccountManager.get(context).getAccountsByType("com.google");
+        return AccountManager.get(context).getAccountsByType(getAccountType());
     }
 }
