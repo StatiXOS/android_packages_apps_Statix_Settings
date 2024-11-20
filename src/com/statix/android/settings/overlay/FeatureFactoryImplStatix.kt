1aecff730dbd9a029cfdb6d81a6bf00f674a12ba
ralph950412 <ralph950412@gmail.com>
2024-11-21 01:06:16 +0800

    speedy
    Change-Id: Ibedf1f7998e774e734efda0b47b0f165843f1fff

--- src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.kt
+++ src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.kt
@@ -1,15 +1,12 @@
 package com.statix.android.settings.overlay
 
-import android.content.Context
-
 import com.android.settings.accounts.AccountFeatureProvider
-import com.android.settings.fuelgauge.BatteryStatusFeatureProvider
 import com.android.settings.overlay.FeatureFactoryImpl
-
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl
 
 class FeatureFactoryImplStatix : FeatureFactoryImpl() {
-    override val accountFeatureProvider: AccountFeatureProvider by lazy {
-        AccountFeatureProviderGoogleImpl()
-    }
+  override val accountFeatureProvider: AccountFeatureProvider by lazy {
+    AccountFeatureProviderGoogleImpl()
+    WifiFeatureProviderStatixImpl()
+  }
 }
---
+++ src/com/statix/android/settings/wifi/factory/WifiFeatureProviderStatixImpl.java
@@ -0,0 +1,25 @@
+package com.statix.android.settings.wifi.factory;
+
+import android.content.Context;
+import com.android.settings.wifi.dpp.WifiDppQrCodeGeneratorFragment;
+import com.android.settings.wifi.factory.WifiFeatureProvider;
+import com.statix.android.settings.flags.Flags;
+import com.statix.android.settings.wifi.dpp.WifiDppQrCodeGeneratorFragmentStatixImpl;
+import kotlin.jvm.internal.Intrinsics;
+/* compiled from: WifiFeatureProviderGoogleImpl.kt */
+/* loaded from: classes3.dex */
+public final class WifiFeatureProviderStatixImpl extends WifiFeatureProvider {
+    /* JADX WARN: 'super' call moved to the top of the method (can break code semantics) */
+    public WifiFeatureProviderStatixImpl(Context appContext) {
+        super(appContext);
+        Intrinsics.checkNotNullParameter(appContext, "appContext");
+    }
+
+    @Override // com.android.settings.wifi.factory.WifiFeatureProvider
+    public WifiDppQrCodeGeneratorFragment getWifiDppQrCodeGeneratorFragment() {
+        if (Flags.enablePrismQrForWifiSharing()) {
+            return new WifiDppQrCodeGeneratorFragmentStatixImpl();
+        }
+        return new WifiDppQrCodeGeneratorFragment();
+    }
+}
