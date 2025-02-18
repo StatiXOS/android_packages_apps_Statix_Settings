03646519db3d215836e9177057aef8ee12b5eee0
ralph950412 <ralph950412@gmail.com>
2025-02-18 12:54:23 +0800

    StatixSettings: Implement SignatureVerifier and minimal SearchFeatureProviderGoogleImpl
    Change-Id: Iaad431558bb55937e43bd693438ed416d22f55bc

--- src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.kt
+++ src/com/statix/android/settings/overlay/FeatureFactoryImplStatix.kt
@@ -3,13 +3,16 @@ package com.statix.android.settings.overlay
 import android.content.Context
 
 import com.android.settings.accounts.AccountFeatureProvider
-import com.android.settings.fuelgauge.BatteryStatusFeatureProvider
 import com.android.settings.overlay.FeatureFactoryImpl
+import com.android.settings.search.SearchFeatureProvider
 
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl
+import com.google.android.settings.search.SearchFeatureProviderGoogleImpl
 
 class FeatureFactoryImplStatix : FeatureFactoryImpl() {
     override val accountFeatureProvider: AccountFeatureProvider by lazy {
         AccountFeatureProviderGoogleImpl()
     }
+
+    override val searchFeatureProvider: SearchFeatureProvider = SearchFeatureProviderGoogleImpl()
 }
